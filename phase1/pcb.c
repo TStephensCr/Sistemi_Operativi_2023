967fe9bd5e4ce464d74c9d14899b9d20658d356d
TStephensCr <creator@Cr34.home-life.hub>
2023-12-06 11:44:33 +0100

    Non ho risolto na sega

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -87,7 +87,7 @@ int emptyChild(pcb_t *p) {
 void insertChild(pcb_t *prnt, pcb_t *p) {
     list_add(&p->p_list, &prnt->p_child);
     p->p_parent = prnt;
-    p->p_sib.next = &prnt->p_child;
+    //fin qui va bene ma c'è un bordello per p_sib, è quello su cui da errore
 }
 
 pcb_t *removeChild(pcb_t *p) {
