9808b31928e8682dc85cc0c4710491a5ef791b5a
greve-luchese <gigiotto36@gmail.com>
2024-03-13 20:24:17 +0100

    ho iniziato a scrivere e ho subito hittato un roadblock con una roba consigliata dal prof :D

--- phase2Umps3/interrupts.c
+++ phase2Umps3/interrupts.c
@@ -1,1 +1,13 @@
-#include "interrupts.h"
+#include "const.h"
+//sta roba dell'and non ha senso...
+int interrupt_line(/*interrupt*/){
+    if(getCAUSE() /*&&interrupt*/){
+        
+    }
+}
+
+void interrupt_eh(){
+    //controlli tutte le lines?? il prof dice che determini le lines con il bitwise and ma quello lo fai su tutte le lines bruh
+    //una volta determinate le lines che hanno un interrupt prendi quello con priorità maggiore 
+    //NB terminal transmission >> terminal receipt
+}
--- phase2Umps3/interrupts.h
+++
@@ -1,6 +0,0 @@
-struct interrupt_exception_handler{
-
-};
-
-void NTI();//non timer interrupts
-void PLT();//processor local timer interrupt
