262d2a47b30cd252b998cb88c07a4e81d90a290d
TStephensCr <creator@Cr34.home-life.hub>
2024-03-13 14:01:53 +0100

    Fatto funzione scheduler, forse manca un dettaglio nell'ultimo caso

---
+++ phase2Umps3/scheduler.c
@@ -0,0 +1,17 @@
+#include "/usr/include/umps3/umps/libumps.h"
+
+void scheduler(){
+    currentProcess = removeProcQ(&readyQueue);
+    if(currentProcess != NULL){
+        currentProcess->p_s.status = currentProcess->p_s.status | TEBITON;
+        setTIMER(TIMESLICE);
+        LDST(&currentProcess->p_s);
+    }
+    if(processCount == 1){
+        HALT();
+    }
+    if(processCount > 1 && softBlockCount > 0){
+        setSTATUS ((getSTATUS () | IECON | IMON) & ~TEBITON);
+        WAIT();
+    } 
+}
