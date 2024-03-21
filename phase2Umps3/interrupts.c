a89286d995f87c01dd661dbcf56a0abfe582c576
greve-luchese <gigiotto36@gmail.com>
2024-03-21 15:42:02 +0100

    ripartito da 0 con gli interrupt

--- phase2Umps3/interrupts.c
+++ phase2Umps3/interrupts.c
@@ -17,19 +17,28 @@ cpu_t tempopassato(){
     return risultante;
 }
 
-//DETERMINA IL TIPO DI INTERRUPT E ASSEGNA 
+//DETERMINA CHE TIPO DI INTERRUPT è PENDING IN BASE ALLA LINEA
+//POI CONTROLLA GLI 8 DEVICE PER CAPIRE A QUALE APPARTIENE
+// 
 void interrupthandler(){
     startinterrupt();
-    if(getCAUSE() && LOCALTIMERINT){//PLT INTERRUPT
-        currentProcess->p_sib = EXCEPTION_STATE;
-        currentProcess->p_time += tempopassato();
-        insertProcQ(&readyQueue, currentProcess);
-        currentProcess = NULL;
-    } //lascia in questo ordine per la priorità
-    else if(getCAUSE() && TIMERINTERRUPT)//INTERVAL TIMER INTERRUPT
-        IT_handler();
-    else
-        NT_handler();
+    if(getCAUSE() && LOCALTIMERINT){//line 1
+        // currentProcess->p_sib = EXCEPTION_STATE;
+        // currentProcess->p_time += tempopassato();
+        // insertProcQ(&readyQueue, currentProcess);
+        // currentProcess = NULL;
+        address = get_numdevice(1);
+    }                                               //lascia in questo ordine per la priorità
+    else if(getCAUSE() && TIMERINTERRUPT)//line 2
+        address = get_numdevice(2);
+    else if(getCAUSE() && DISKINTERRUPT)//line 3
+        address = get_numdevice(3);
+    else if(getCAUSE() && FLASHINTERRUPT)//line 4
+        address = get_numdevice(4);
+    else if(getCAUSE() && PRINTINTERRUPT)//line 5
+        address = get_numdevice(5);
+    else if(getCAUSE() && TERMINTERRUPT)// line 6
+        address = get_numdevice(6);
     endinterrupt();
 }
 
@@ -57,46 +66,44 @@ void endinterrupt(){
 
 void NT_handler(int ip){
     int line, numero, mask;
-    devAddrBase = get_numdevice(line);
+    int devAddrBase = get_numdevice(line);
     status = get_status(devAddrBase);
     set_status(ACK);
     /**
-     * manda messaggio e sblocca il pb in attes di questo device
+     * manda messaggio e sblocca il pcb in attes di questo device
      * aggiorna il registro v0 del pcb con status
      * metti il pcb sbloccato nella ready queue cambiando lo state da blocked a ready
      * ridai il controllo al current process facendo un LDST sull'exception state salvato (si trova all'inizio della BIOSDATAPAGE)
     */
 }
 
-void IT_handler(){
-   
-}
 
 int get_numdevice(int line){
     switch (line){
         case DEV1ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (1 * 0x10);        
+            return int devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (1 * 0x10);        
             break;
         case DEV2ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (2 * 0x10);
+            return devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (2 * 0x10);
             break;
         case DEV3ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (3 * 0x10);       
+            return devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (3 * 0x10);       
             break;
         case DEV4ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (4 * 0x10);       
+            return devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (4 * 0x10);       
             break;
         case DEV5ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (5 * 0x10);       
+            return devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (5 * 0x10);       
             break;
         case DEV6ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (6 * 0x10);       
+            return devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (6 * 0x10);       
             break;
         case DEV7ON:
-            devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (7 * 0x10);       
+            return devAddrBase = 0x10000054 + ((IntlineNo - 3) * 0x80) + (7 * 0x10);       
             break;
         
     default:
         break;
     }
+    
 }
