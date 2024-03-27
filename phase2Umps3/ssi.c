6d0e308d0c02b3568d1d60603d428a0452f9775f
TStephensCr <creator@Cr34.home-life.hub>
2024-03-27 10:56:18 +0100

    Messo su uno scheletro di SSIRequest

--- phase2Umps3/ssi.c
+++ phase2Umps3/ssi.c
@@ -1,13 +1,47 @@
-#include "./headers/ssi.h"
+#include "/usr/include/umps3/umps/libumps.h"
 
 void SSIRequest(pcb_t* sender, int service, void* ar){
-    
+    switch(service){
+        case 1:
+            createProcess();
+            break;
+        case 2:
+            terminateProcess((pcb_t*)ar);
+            break;
+        case 3:
+            //DOIO
+            break;
+        case 4:
+            return sender->p_time;
+            break;
+        case 5:
+            //put sender in WAIT until next pseudo-clock tick, and keep a list of those waiting
+            break;
+        case 6:
+            return sender->p_supportStruct;
+            break;
+        case 7;
+            if(ar == 0)
+                return sender->p_pid;
+            if(sender->p_parent != NULL)
+                return sender->p_parent->p_pid;
+            return 0;
+    }
 }
 
 void remoteProcedureCall(){
     while(TRUE){
     //receive request
-    void SSIRequest(pcb_t* sender, int service, void* arg);//satisfy request
+    SSIRequest(pcb_t* sender, int service, void* arg);//satisfy request
     //send back results
     }
 }
+
+pcb_t* createProcess(){
+
+}
+
+void terminateProcess(pcb_t* process){
+
+}
+
