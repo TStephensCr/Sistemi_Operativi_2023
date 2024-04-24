9186eebe8be5a735aae9ef222158c14c1c6b401b
MatteoPanicciari <matteo.panicciari@gmail.com>
2024-04-24 15:23:07 +0200

    scaricate le librerie giusto per vederle e per far makare gianlu, poi alla fine le eliminiamo

--- phase2Umps3/headers/initial.h
+++ phase2Umps3/headers/initial.h
@@ -1,18 +1,27 @@
 #ifndef INITIAL_H_INCLUDED
 #define INITIAL_H_INCLUDED
 
-#include "../../headers/types.h"
-#include "../../headers/const.h"
-#include "../../phase1/headers/pcb.h"
-#include "../../phase1/headers/msg.h"
-#include "/usr/include/umps3/umps/libumps.h"
+/*#include "/usr/include/umps3/umps/libumps.h"
 #include "/usr/include/umps3/umps/const.h"
 #include "/usr/include/umps3/umps/const.h"
 #include "/usr/include/umps3/umps/cp0.h"
 #include "/usr/include/umps3/umps/aout.h"
 #include "/usr/include/umps3/umps/arch.h"
 #include "/usr/include/umps3/umps/bios_defs.h"
-#include "/usr/include/umps3/umps/types.h"
+#include "/usr/include/umps3/umps/types.h"*/
+#include "./../../librerie/libumps.h"
+#include "./../../librerie/const.h"
+#include "./../../librerie/const.h"
+#include "./../../librerie/cp0.h"
+#include "./../../librerie/aout.h"
+#include "./../../librerie/arch.h"
+#include "./../../librerie/bios_defs.h"
+#include "./../../librerie/types.h"
+
+#include "../../headers/types.h"
+#include "../../headers/const.h"
+#include "../../phase1/headers/pcb.h"
+#include "../../phase1/headers/msg.h"
 
 #include "./scheduler.h"
 #include "./exceptions.h"
